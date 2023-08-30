e56cedf8e5fb6849d3fea7c34bd9ce208ca420d7
Diyar Ozan Tekin <76197104+tekinozan@users.noreply.github.com>
2023-08-30 22:22:33 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "0x99145aA71F6046Ae4A76953dD79a0cfB69EDD83d";
+export const ROUTER_ADDRESS = "YOUR ROUTER ADDRESS";
 //export const POOLS = undefined; 
 //export const COIN_NAME = "WETH";
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/tHXBu6R9-Gp0I2_yg9pHdwAg47QRVyv3",
+    [Goerli.chainId]: "YOUR_PROVIDER_LINK", // can take provider link in alchemy.com
   },
 };
